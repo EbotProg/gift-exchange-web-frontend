63969c4863f75bd93b1c60e0e977887df27ac210
Achale Ebot <achaleebotoma2002@gmail.com>
2024-12-14 14:57:57 +0100

    updated code

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -5,7 +5,7 @@ const Success = () => {
     <div className="grid gap-3 place-content-center h-screen font-redHatText p-3">
       <div className="border px-6 py-12 rounded-lg flex flex-col gap-2">
         <h1 className=" text-6xl font-bold">Success🎉🎉🎉</h1>
-        <p>You have successfully chosen the receiver of you gift</p>
+        <p>You have successfully chosen the receiver of your gift</p>
       </div>
     </div>
   );
